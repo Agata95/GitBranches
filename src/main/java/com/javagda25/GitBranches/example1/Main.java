dc1c21b4d814f6b214839cb0a3e762a37b793075
Agata <agata.weltrowska@outlook.com>
2019-06-11 18:12:41 +0200

    first example

--- src/main/java/com/javagda25/GitBranches/example1/Main.java
+++ src/main/java/com/javagda25/GitBranches/example1/Main.java
@@ -1,7 +1,18 @@
 package com.javagda25.GitBranches.example1;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
+        // data.txt - Hello World! - w trybie append
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true))) {
+            printWriter.println("Hello World!");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
