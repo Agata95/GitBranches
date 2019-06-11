e270dc03f291ff0894f4eaf2aff2df76a284bc4a
Agata <agata.weltrowska@outlook.com>
2019-06-11 18:29:24 +0200

    new branch

--- src/main/java/com/javagda25/GitBranches/example1/Main.java
+++ src/main/java/com/javagda25/GitBranches/example1/Main.java
@@ -1,7 +1,28 @@
 package com.javagda25.GitBranches.example1;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true))) {
+            printWriter.println("Hello World!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            String linia;
+            int licznik = 0;
+
+            while ((linia = reader.readLine()) != null) {
+                licznik++;
+            }
+            System.out.println(licznik);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
